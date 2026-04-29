f84eaef6223162dd8161b45b98ac085f5b3ac758
Talal2003 <www.talal2003@gmail.com>
2026-04-28 23:30:16 -0400

    Add 90° and 180° step moves with delays using Stepper library

--- automated_polarized_sunlight-blocking_film_system.ino
+++ automated_polarized_sunlight-blocking_film_system.ino
@@ -1,28 +1,29 @@
-#include <AccelStepper.h>
+#include <Stepper.h>
 
-#define MotorInterfaceType AccelStepper::HALF4WIRE
+// Steps per full revolution
+const int stepsPerRevolution = 2048;
 
-// Pins IN1, IN3, IN2, IN4
-AccelStepper stepper(MotorInterfaceType, 52, 51, 53, 50);
+// Step amounts
+const int steps90  = stepsPerRevolution / 4;  // 90 degrees
+const int steps180 = stepsPerRevolution / 2; // 180 degrees
+
+// ULN2003 pins: IN1, IN3, IN2, IN4
+Stepper stepper(stepsPerRevolution, 52, 51, 53, 50);
 
 void setup() {
-  stepper.setMaxSpeed(800);
-  stepper.setAcceleration(400);
+  stepper.setSpeed(10); // RPM
 }
 
 void loop() {
   // 90° clockwise
-  stepper.move(2048);
-  stepper.runToPosition();
-
+  stepper.step(steps90);
+  delay(1000);
 
   // 180° counter-clockwise
-  stepper.move(-2048);
-  stepper.runToPosition();
+  stepper.step(-steps180);
+  delay(1000);
 
-
-  // 90° clockwise (back to start)
-  stepper.move(2048);
-  stepper.runToPosition();
-
+  // 90° clockwise
+  stepper.step(steps90);
+  delay(1000);
 }
