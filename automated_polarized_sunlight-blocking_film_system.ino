911cdc99725c3bdc97988d54e3d1ca4a8a9ecc62
Talal2003 <www.talal2003@gmail.com>
2026-04-28 23:34:14 -0400

    Add light sensor control for 90° and 180° stepper moves

--- automated_polarized_sunlight-blocking_film_system.ino
+++ automated_polarized_sunlight-blocking_film_system.ino
@@ -1,35 +1,50 @@
 #include <Stepper.h>
 
 const int stepsPerRevolution = 2048;
-
-// Stepper motor pins
 Stepper stepperName(stepsPerRevolution, 52, 51, 53, 50);
 
-// Button pins
-const int button90Pin  = 38;
-const int button180Pin = 39;
+const int lightPin = A0;
 
-void setup() {
-  stepperName.setSpeed(5);
+// Light ranges
+const int noLightThreshold  = 300;
+const int highLightThreshold = 700;
 
-  pinMode(button90Pin, INPUT_PULLUP);
-  pinMode(button180Pin, INPUT_PULLUP);
-}
+// Track last state so it doesn't repeat
+int lastState = 0;
 
-// Convert degrees to steps
 int degreesToSteps(int degrees) {
   return (degrees * stepsPerRevolution) / 360;
 }
 
+void setup() {
+  stepperName.setSpeed(5);
+  Serial.begin(9600);
+}
+
 void loop() {
+  int lightValue = analogRead(lightPin);
+  Serial.println(lightValue);
 
-  if (digitalRead(button90Pin) == LOW) {
-    stepperName.step(stepsPerRevolution / 4);
-    delay(300);
+  // BELOW 300, do nothing
+  if (lightValue < noLightThreshold) {
+    lastState = 0;   // reset state so it can trigger again later
   }
 
-  if (digitalRead(button180Pin) == LOW) {
-    stepperName.step(-(stepsPerRevolution / 4));
-    delay(300);
+  // BETWEEN 300 AND 700, 90 degrees
+  else if (lightValue >= noLightThreshold && lightValue <= highLightThreshold) {
+    if (lastState != 1) {
+      stepperName.step(stepsPerRevolution / 4);
+      lastState = 1;
+    }
   }
+
+  // ABOVE 700, 180 degrees
+  else if (lightValue > highLightThreshold) {
+    if (lastState != 2) {
+      stepperName.step(stepsPerRevolution / 2);
+      lastState = 2;
+    }
+  }
+
+  delay(100);
 }
